7b938cdfb9d1ef435432d184a1d04e075c606a61
dodriguez <acounts.drodriguez@gmail.com>
2021-05-25 08:43:55 -0500

    temas: slector id, selector por clases, slector por etiquetas, selector por atributos y metodos find y parent

--- jquery/js/selectores.js
+++ jquery/js/selectores.js
@@ -12,6 +12,36 @@ $(document).ready(function(){
 
 
     //selector de una clase
-    var miClase = $(".zebra") //.eq(1) selecciona un elemento d eun array
+    var miClase = $(".zebra").css("padding", "5px"); //.eq(1) selecciona un elemento d eun array
     //miClase.css("border", "5px dashed black")
+
+    $('.sinBorder').click(function(){
+        console.log("click");
+        $(this).addClass('zebra');
+    });
+
+    //selectores de etiqueta
+    var parrafos = $('p').css('cursor', 'pointer'); // idea para quitar scroll
+    parrafos.click(function(){
+        var pThis = $(this); //cargar el dom
+        if (!pThis.hasClass('grande')) {
+            pThis.addClass('grande') //siempre trbajar con comillas simples
+            
+        }else{
+            pThis.removeClass('grande')
+        }
+        //$(this).removeClass("zebra")
+    });
+
+    //selectores de atributos
+    $('[titel="Google"]').css('background', '#cc'),
+    $('[title="Facebook"]').css('background', 'blue');
+
+    //otros
+    //$('p,a').addClass('margen-superior');
+    var search = $("#caja").find('.result'); //forma 1
+    //var search = $("#caja .result"); // forma 2
+    var srh = $("#elemento2").parent().parent().find('.result');
+    console.log(srh); //find() busca en todo el selector dado
+                    // parent() da un paso atras del elemento dado ..
 });
